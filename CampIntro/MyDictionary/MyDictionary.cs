40039da9a6bcad79e3838f76cd18d8359bc98d36
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-07-25 20:03:46 +0300

    Add myDisctionary

---
+++ CampIntro/MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+
+        }
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = keys;
+            Value[] tempValues = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+        }
+
+        public int Count
+        {
+            get { return keys.Length;  }
+        }
+        public Key[] key
+        {
+            get { return keys; }
+        }
+        public Value[] value
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ CampIntro/MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> users = new MyDictionary<int, string>();
+            users.Add(1, "Hakan");
+            users.Add(2, "Betul");
+            
+            foreach (var item in users.value)
+            {
+                Console.WriteLine(item);
+                
+            }
+            Console.WriteLine("Number of elements => " + users.Count);
+        }
+    }
+}
