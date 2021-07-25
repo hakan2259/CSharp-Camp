7fd6c75dada7a1fdbabecf9ab3cb7ab4572ba20c
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-07-25 19:16:30 +0300

    Add constructors example

---
+++ CampIntro/Constructors/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public Customer()
+        {
+            
+        }
+        public Customer(int id,string firstname,string lastname,string city)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            City = city;
+
+        }
+       
+    }
+}
---
+++ CampIntro/Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Hakan", LastName = "Sandal", City = "Tekirdağ" };
+
+
+            Customer customer2 = new Customer(2, "Betul", "Yesildag", "Tekirdağ");
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
