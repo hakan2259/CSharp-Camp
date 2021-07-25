d522e10dc4341c924ac114ab946ef0f1e465af89
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-07-25 18:24:15 +0300

    Add GenericsIntro

---
+++ CampIntro/Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names1 = new List<string> { "Hakan","Betul","Merve","Sedef"};
+
+            Console.WriteLine(names1[0]);
+            Console.WriteLine(names1[1]);
+            Console.WriteLine(names1[2]);
+            Console.WriteLine(names1[3]);
+            names1.Add("Defne");
+            Console.WriteLine(names1[4]);
+            
+
+        }
+    }
+}
---
+++ CampIntro/GenericsIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Hakan");
+            Console.WriteLine(names.Length);
+            names.Add("Betül");
+            Console.WriteLine(names.Length);
+
+            foreach(var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
