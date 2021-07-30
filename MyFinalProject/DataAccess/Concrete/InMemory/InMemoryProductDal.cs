da8c968f4c970c49f99f5d9fac12ab4e69a9f7c5
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-07-30 03:04:51 +0300

    Add project

---
+++ MyFinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,91 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle, Sql Server, Postgres, MongoDb
+            _products = new List<Product>
+            {
+                new Product{
+                    ProductId=1,
+                    CategoryId=1,
+                    ProductName="Fincan",
+                    UnitsInStock=15,
+                    UnitPrice=75
+                },
+                new Product{
+                    ProductId=2,
+                    CategoryId=1,
+                    ProductName="Bardak",
+                    UnitsInStock=10,
+                    UnitPrice=20
+                },
+                new Product{
+                    ProductId=3,
+                    CategoryId=2,
+                    ProductName="Laptop",
+                    UnitsInStock=3,
+                    UnitPrice=3999
+                },
+                   new Product{
+                    ProductId=4,
+                    CategoryId=2,
+                    ProductName="Phone",
+                    UnitsInStock=5,
+                    UnitPrice=7500
+                },
+                    new Product{
+                    ProductId=5,
+                    CategoryId=2,
+                    ProductName="Klavye",
+                    UnitsInStock=20,
+                    UnitPrice=50
+                },
+
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+        //LINQ - Language Integrated Query
+        //Lambda
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
