cc81d18b9db2c0adf18223d7b627337b4bd0c049
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-08-09 22:11:08 +0300

    Add entity framework

---
+++ MyFinalProject/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : reference type
+    //IEntity : IEntity olabilir veya IEntity implemente eden nesne olabilir.
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
