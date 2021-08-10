35784f9797e78e01b94558e502734cba628d67bd
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-08-10 18:54:37 +0300

    Add ReCapProject

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            /*carManager.Add(new Car
+            {
+                BrandId = 14,
+                ColorId = 2,
+                ModelYear = 2020,
+                DailyPrice = 1200,
+                Description = "S90 2.0 B5 Inscription"
+            });*/
+
+            foreach (var car in carManager.GetCarsByBrandId(4))
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("******************************");
+            foreach (var car in carManager.GetCarsByColorId(1))
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+            
+            
+        }
+    }
+}
