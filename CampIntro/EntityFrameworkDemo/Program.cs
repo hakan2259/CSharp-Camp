7d0a324279e5370fcae141a584d4e461c8a8d523
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-08-10 21:33:50 +0300

    Add entity framework

---
+++ CampIntro/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADO.NET
+            //Entity Framework --Bir ORM - Object relational mapping
+            //NHibernate
+            //Dapper
+
+            //GetAll();
+            GetProductsByCategory(1);
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            foreach (var product in context.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext context = new NorthwindContext();
+            var result = context.Products.Where(p => p.CategoryId == categoryId);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
