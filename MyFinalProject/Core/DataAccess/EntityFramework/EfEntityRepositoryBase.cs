8ae57d1aaa35528230842d3ca841fcc80402992d
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-08-10 22:47:58 +0300

    Add core layer

--- MyFinalProject/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ MyFinalProject/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstract
+namespace Core.DataAccess.EntityFramework
 {
-    // IEntity implement eden class bir veritabanı tablosudur.
-    public interface IEntity
+    public class EfEntityRepositoryBase<TEntity,TContext>
     {
+
     }
 }
