29b469e8243e408d887d0c23b58193dcb55f9ded
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-08-11 22:06:13 +0300

    Update project

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,11 @@ namespace Business.Concrete
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
 
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
--- MyFinalProject/ConsoleUI/Program.cs
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -12,13 +12,35 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //DTO => Data Transformation Object
+            //ProductTest();
+            //IoC
+            //CategoryTest();
+
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(20,100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
 
+        }
 
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(20, 100))
+            {
+                Console.WriteLine(product.ProductName +  "/" + product.CategoryId);
+            }
         }
     }
 }
